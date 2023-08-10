7cd9d3bd9ea043525a2b02a1dd5d5d529b665a0f
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-08-10 16:28:01 +0200

    Initial Commit

---
+++ clubSS/clubSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  clubSSApp.swift
+//  clubSS
+//
+//  Created by Stjepan Stojčević on 10.08.2023..
+//
+
+import SwiftUI
+
+@main
+struct clubSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

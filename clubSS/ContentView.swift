8e749c6aca0df594e5da4a7124f50a20a09bbd48
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-08-10 16:30:42 +0200

    init

--- clubSS/ContentView.swift
+++ clubSS/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var clubIndex = 0
+    @State private var cityIndex = 0
+    @State private var singerIndex = 0
+    var clubs = ["Aqua","Ledana","Q","Deja vu","Queen","Platinum"]
+    var cities = ["Biograd","Zadar","Zagreb","Požega"]
+    var singers = ["Voyage","Nucci","Jala Brat","Buba Corelli","Fox","Grše"]
+    
+    var body: some View
+    {
+        NavigationView
+        {
+            Form
+            {
+                Section
+                {
+                    Picker(selection: $cityIndex, label: Text("City")
+                        .foregroundColor(Color.red))
+                    {
+                        ForEach(cities.indices, id: \.self)
+                        {
+                            Text(self.cities[$0])
+                        }
+                    }
+                }
+                Section
+                {
+                    Picker(selection: $clubIndex, label: Text("Club")
+                        .foregroundColor(Color.blue))
+                    {
+                        ForEach(clubs.indices, id: \.self)
+                        {
+                            Text(self.clubs[$0])
+                        }
+                    }
+                }
+                
+                Section
+                {
+                    Picker(selection: $singerIndex, label: Text("Singer")
+                        .foregroundColor(Color.orange))
+                    {
+                        ForEach(singers.indices, id: \.self)
+                        {
+                            Text(self.singers[$0])
+                        }
+                    }
+                }
+            }.navigationTitle(Text("Partyyy...🥳"))
         }
-        .padding()
     }
 }
 
@@ -24,3 +65,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
